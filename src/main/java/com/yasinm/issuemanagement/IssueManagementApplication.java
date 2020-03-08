69c42389a5392434aeae7a49acae2f95c64ac78e
Yasin Memic <yasin.memic@hotmail.com>
2020-03-08 13:19:40 +0300

    Exception Handler and Postgres Database Configuration were added.

--- src/main/java/com/yasinm/issuemanagement/IssueManagementApplication.java
+++ src/main/java/com/yasinm/issuemanagement/IssueManagementApplication.java
@@ -5,10 +5,12 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
+@EnableJpaRepositories
 public class IssueManagementApplication {
 
     public static void main(String[] args) {
